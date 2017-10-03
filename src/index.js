74238da76b2f290346e7d6d3358c8098510edc56
Khoa Nguyen <henrykhoanguyen@gmail.com>
2017-10-02 20:58:43 -0700

    change index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const App = () => {
+    
+    
+    return (
+        <div>
+        <h1> HELLO THERE WORLD</h1>
+        <p> I love saying hello </p>
+        </div>
+    )
+}
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
