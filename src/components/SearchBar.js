dfe086e7e36deaef1aeacb716df147664e7b88ea
Khoa Nguyen <henrykhoanguyen@gmail.com>
2017-10-09 21:02:50 -0700

    completed Search Bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import 'font-awesome/css/font-awesome.css'
+import './SearchBar.css'
+import PropTypes from 'prop-types'
 
 class SearchBar extends React.Component {
     
@@ -17,6 +20,12 @@ class SearchBar extends React.Component {
         })
     }
 
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.updateSearchTerm(this.state.search)
+
+    }
+
     render(){
         return (
             <div className="searchbar-contianer">
@@ -30,10 +39,21 @@ class SearchBar extends React.Component {
                         onChange = {this.handleSearchChange}
                     />
 
+                    <button 
+                        type="submit"
+                        className="search-button"
+                        onClick={this.handleSubmit}
+                    >
+                        <i className = "search-icon fa fa-search"/>
+                    </button>
                 </form>
             </div>
         )
     }
+}
+
+PropTypes.PropTypes = {
+    updateSearchTerm: PropTypes.func.isRequired
 }
 
 export default SearchBar
